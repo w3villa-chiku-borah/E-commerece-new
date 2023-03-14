b297dc74373ef8885cc490d5a93b9863f3492649
CHIKU BORAH <chiku.borah@w3villa.com>
2023-03-14 17:51:08 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-async function readData (){
+async function readData() {
     const response = await fetch('./data/fashion.json');
     const data = await response.json();
     const response2 = await fetch('./data/mostView.json');
     const data2 = await response2.json();
-   
+
     showFashion(data.fashionProducts);
-    showMostView (data2.mostView);
-   
+    showMostView(data2.mostView);
+
     // showBlog (data3.blog2)
 }
 
-const showFashion = (arrayOfData) =>{
+const showFashion = (arrayOfData) => {
     let html = `<div class="owl-carousel owl-theme" id="owlfour">`;
     let fashionContainer = document.getElementById("show-fashion");
     arrayOfData.forEach(element => {
@@ -58,40 +58,39 @@ const showFashion = (arrayOfData) =>{
     fashionContainer.innerHTML = html;
 
     $('#owlfour').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:false,
+        loop: true,
+        margin: 0,
+        nav: false,
         // autoplay:true,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1084:{
-                items:2
+            1084: {
+                items: 2
             },
-        
-            1220:{
-                items:2
+
+            1220: {
+                items: 2
             },
-            
-        
-            1400:{
-                items:3
+
+
+            1400: {
+                items: 3
             },
         }
-        
-        });
-    
+
+    });
 }
- 
-const showMostView = (arrayOfData) =>{
+
+const showMostView = (arrayOfData) => {
     let html = `<div class="owl-carousel owl-theme" id="owlnine">`;
-    let mView= document.getElementById("mostView");
+    let mView = document.getElementById("mostView");
     arrayOfData.forEach(element => {
- html += `
+        html += `
  <div class="item-09">
                     <div class="item-c-09">
                         <div class="item-c1-09"><img src="${element.img}"></div>
@@ -110,52 +109,52 @@ const showMostView = (arrayOfData) =>{
     html += `</div>`;
     mView.innerHTML = html;
     $('#owlnine.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:false,
-        autoplay:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 0,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             },
-        
-            1220:{
-                items:3
+
+            1220: {
+                items: 3
             },
-        
-            1460:{
-                items:4
+
+            1460: {
+                items: 4
             },
         }
-        
-        });
+
+    });
 
 }
-async function readDataBlog(elem){
+async function readDataBlog(elem) {
     const response3 = await fetch('./data/blog.json');
     const data3 = await response3.json();
-    switch(elem){
-        case "LATEST POST" :
+    switch (elem) {
+        case "LATEST POST":
             showBlog(data3.LATEST_POST);
             // console.log(data4.FEATURED);
-   break;
-   case "MOST READ" :
-    showBlog(data3.MOST_READ);
-   break;
-}
+            break;
+        case "MOST READ":
+            showBlog(data3.MOST_READ);
+            break;
+    }
 }
 
-const showBlog = (arrayOfData) =>{
-    let html=`<div class="owl-carousel owl-theme" id="owlthree">`;
-    let blog= document.getElementById('from-our-blog-img');
+const showBlog = (arrayOfData) => {
+    let html = `<div class="owl-carousel owl-theme" id="owlthree">`;
+    let blog = document.getElementById('from-our-blog-img');
     arrayOfData.forEach(element => {
-html +=`<div class="item item-main-07">
+        html += `<div class="item item-main-07">
 <img src="${element.img}"> 
 <div class="aug">
     2 <br> aug
@@ -183,75 +182,78 @@ html +=`<div class="item item-main-07">
     html += `</div>`;
     blog.innerHTML = html;
     $('#owlthree.owl-carousel').owlCarousel({
-         loop:true,
-        margin:0,
-        nav:false,
-        autoplay:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 0,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:2
+            1000: {
+                items: 2
             },
-        
-            1220:{
-                items:3
+
+            1220: {
+                items: 3
             },
-        
-            1460:{
-                items:3
+
+            1460: {
+                items: 3
             },
         }
-        
-        }); 
+
+    });
 }
- readDataBlog("LATEST POST");
+readDataBlog("LATEST POST");
 
 readData();
 
-async function readData2(elem){
+async function readData2(elem) {
     const response4 = await fetch('./data/featured.json');
     const data4 = await response4.json();
 
     // showFeature(data4.FEATURED);
-    switch(elem){
-        case "FEATURED" :
+    switch (elem) {
+        case "FEATURED":
             showFeature(data4.FEATURED);
             // console.log(data4.FEATURED);
-   break;
-   case "LATEST" :
-    showFeature(data4.LATEST);
-   break;
-   case "BESTSELLERS" :
-    showFeature(data4.BESTSELLERS);
-   break;
+            break;
+        case "LATEST":
+            showFeature(data4.LATEST);
+            break;
+        case "BESTSELLERS":
+            showFeature(data4.BESTSELLERS);
+            break;
 
-    
-   case "SPECIAL" :
-    showFeature(data4.SPECIAL);
-   break;
+
+        case "SPECIAL":
+            showFeature(data4.SPECIAL);
+            break;
 
     }
 }
 
-const showFeature = (arrayOfData) =>{
-    let html=`<div class="owl-carousel owl-theme" id="owltwo">`;
-    let blog= document.getElementById('show-feature');
+const showFeature = (arrayOfData) => {
+    let html = `<div class="owl-carousel owl-theme" id="owltwo">`;
+    let blog = document.getElementById('show-feature');
     arrayOfData.forEach(element => {
         html += `<div class="item02"> <img class="labels-1" src="${element.img}">
-        <div class="lebels-c1">
+         
+        ${element.c_lebels ? `<div class="lebels-c1">
             CUSTOM LEBELS
-        </div>
+        </div>` : ``}
+
         <div class="lebels-c2">
             -70%
         </div>
-        <div class="lebels-c2 lebels-c3">
+       ${element.hot ? `<div class="lebels-c2 lebels-c3">
             HOT
-        </div>
+        </div>` : ``}
+
         <div class="item02-c1">
             <p><u>${element.brand}</u></p>
             <p>model ${element.modal_name}</p>
@@ -273,37 +275,37 @@ const showFeature = (arrayOfData) =>{
                 Buy Now </p>
             <p><i class="fa-regular fa-circle-question"></i> Question</p>
         </div>
-    </div>` 
+    </div>`
     });
     html += `</div>`;
     blog.innerHTML = html;
     $('#owltwo.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        autoplay:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             },
-        
-            1220:{
-                items:4
+
+            1220: {
+                items: 4
             },
-        
-            1460:{
-                items:5
+
+            1460: {
+                items: 5
             },
         }
-        
-        });
-       
+
+    });
+
 }
 
 
@@ -311,67 +313,67 @@ readData2("FEATURED");
 
 
 
-async function readData3(elem){
+async function readData3(elem) {
     const response5 = await fetch('./data/buy.json');
     const data5 = await response5.json();
 
     // showBuy(data5.TOP_CATEGORIES);
-    switch(elem){
-        case "TOP_CATEGORIES" :
+    switch (elem) {
+        case "TOP_CATEGORIES":
             showBuy(data5.TOP_CATEGORIES);
             // console.log(data4.FEATURED);
-   break;
-   case "ELECTRONICS" :
-    showBuy(data5.ELECTRONICS);
-   break;
-   case "BEAUTY" :
-    showBuy(data5.BEAUTY);
-   break;
+            break;
+        case "ELECTRONICS":
+            showBuy(data5.ELECTRONICS);
+            break;
+        case "BEAUTY":
+            showBuy(data5.BEAUTY);
+            break;
 
-    
-   case "FASHION" :
-    showBuy(data5.FASHION);
-   break;
+
+        case "FASHION":
+            showBuy(data5.FASHION);
+            break;
 
     }
 }
 
-const showBuy = (arrayOfData) =>{
-    let html=`<div class="owl-carousel owl-theme" id="owlFirst">`;
-    let blog= document.getElementById("buy-img-container");
+const showBuy = (arrayOfData) => {
+    let html = `<div class="owl-carousel owl-theme" id="owlFirst">`;
+    let blog = document.getElementById("buy-img-container");
     arrayOfData.forEach(element => {
-        html +=   `<div class="item">
+        html += `<div class="item">
         <div class="item-c">${element.type}</div><img src="${element.img}">
     </div>
 `
-});
-html += `</div>`;
-blog.innerHTML = html;
-$('#owlFirst.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        },
-    
-        1220:{
-            items:4
-        },
-    
-        1460:{
-            items:5
-        },
-    }
-    
+    });
+    html += `</div>`;
+    blog.innerHTML = html;
+    $('#owlFirst.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+
+            1220: {
+                items: 4
+            },
+
+            1460: {
+                items: 5
+            },
+        }
+
     });
 
 }
